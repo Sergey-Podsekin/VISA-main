afa4610f9cada0601a3add1de5504828c98f3b2f
Sergey Podsekin <sergeykenzo@gmail.com>
2022-11-05 00:07:59 +0300

    Update FRANCE.py

--- FRANCE.py
+++ FRANCE.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,11 +8,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
-
-options = Options()
-options.headless = True
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+#options = Options()
+#options.headless = True
+#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 
 def send_to_telegram(message):  # Telegram bot
