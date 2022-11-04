ee597492493e0cfb68689cb0038d287d961a1777
Sergey Podsekin <sergeykenzo@gmail.com>
2022-11-05 01:45:08 +0300

    Update FRANCE.py

--- FRANCE.py
+++ FRANCE.py
@@ -45,47 +45,53 @@ def send_to_telegram_log(message):  # Telegram bot
 
 count = 1
 
-driver.get("https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
-           "%20de%20visa")
+def checker():
+    global count
+    driver.get("https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
+               "%20de%20visa")
 
-driver.implicitly_wait(20)
-driver.find_element(By.XPATH, '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
+    driver.implicitly_wait(20)
+    driver.find_element(By.XPATH, '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
 
-driver.implicitly_wait(20)
-driver.find_element(By.XPATH, '//div[4]/button').click()
+    driver.implicitly_wait(20)
+    driver.find_element(By.XPATH, '//div[4]/button').click()
 
-driver.implicitly_wait(20)
-driver.find_element(By.CLASS_NAME, 'custom-control-label').click()
+    driver.implicitly_wait(20)
+    driver.find_element(By.CLASS_NAME, 'custom-control-label').click()
 
-driver.implicitly_wait(20)
-driver.find_element(By.XPATH, '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
+    driver.implicitly_wait(20)
+    driver.find_element(By.XPATH, '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
+
+    while True:
+
+        try:
+            elem = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, '//p[@class="lead fr-text mt-4 mb-3 text-center"]')))
+
+            driver.get(
+                "https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
+                "%20de%20visa")
+            driver.implicitly_wait(20)
+            driver.find_element(By.XPATH,
+                                '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
+
+            driver.implicitly_wait(20)
+            driver.find_element(By.XPATH, '//button[@class ="btn btn-primary btn-md"]').click()
+
+            driver.implicitly_wait(20)
+            driver.find_element(By.XPATH,
+                                '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
+            if count % 10 == 0:
+                driver.quit()
+                break
+        finally:
+            send_to_telegram(
+                "Свободные места на визу Франция https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
+                "%20de%20visa")
+            driver.save_screenshot(f'WARNING!!!!!{count}.png')
+
+        send_to_telegram_log(count)
+        count = count + 1
 
 while True:
-
-    try:
-        time.sleep(3)
-        elem = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, '//p[@class="lead fr-text mt-4 mb-3 text-center"]')))
-
-        driver.get(
-            "https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
-            "%20de%20visa")
-        driver.implicitly_wait(20)
-        driver.find_element(By.XPATH,
-                            '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
-
-        driver.implicitly_wait(20)
-        driver.find_element(By.XPATH, '//button[@class ="btn btn-primary btn-md"]').click()
-
-        driver.implicitly_wait(20)
-        driver.find_element(By.XPATH,
-                            '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
-
-    finally:
-        send_to_telegram(
-            "Свободные места на визу Франция https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
-            "%20de%20visa")
-        driver.save_screenshot(f'WARNING!!!!!{count}.png')
-
-    send_to_telegram_log(count)
-    count = count + 1
+    checker()
