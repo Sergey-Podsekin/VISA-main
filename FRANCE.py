fd5d068ab0d93425f96a937f3df2eb6b6b51cf48
Sergey Podsekin <sergeykenzo@gmail.com>
2022-11-05 18:08:55 +0300

    Update FRANCE.py

--- FRANCE.py
+++ FRANCE.py
@@ -4,18 +4,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
-import time
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+#driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
-#options = Options()
+#options = webdriver.ChromeOptions()
 #options.headless = True
-#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+#driver = webdriver.Chrome(executable_path="C:\PYTHON\chromedriver.exe", options=options)
 
 
 def send_to_telegram(message):  # Telegram bot
@@ -46,52 +46,37 @@ def send_to_telegram_log(message):  # Telegram bot
 count = 1
 
 def checker():
+
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     global count
     driver.get("https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
                "%20de%20visa")
 
     driver.implicitly_wait(20)
-    driver.find_element(By.XPATH, '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
+    driver.find_element(By.CSS_SELECTOR, "button[class='fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left ']").click()
 
     driver.implicitly_wait(20)
-    driver.find_element(By.XPATH, '//div[4]/button').click()
+    driver.find_element(By.XPATH, "//button[normalize-space()='Confirmer']").click()
 
     driver.implicitly_wait(20)
-    driver.find_element(By.CLASS_NAME, 'custom-control-label').click()
+    driver.find_element(By.CSS_SELECTOR, "label[for='readInformations']").click()
 
     driver.implicitly_wait(20)
-    driver.find_element(By.XPATH, '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
+    driver.find_element(By.CSS_SELECTOR, "button[class='fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left ']").click()
 
-    while True:
-
-        try:
-            elem = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.XPATH, '//p[@class="lead fr-text mt-4 mb-3 text-center"]')))
-
-            driver.get(
-                "https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
-                "%20de%20visa")
-            driver.implicitly_wait(20)
-            driver.find_element(By.XPATH,
-                                '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
-
-            driver.implicitly_wait(20)
-            driver.find_element(By.XPATH, '//button[@class ="btn btn-primary btn-md"]').click()
-
-            driver.implicitly_wait(20)
-            driver.find_element(By.XPATH,
-                                '//button[@class="fr-btn fr-btn--primary fr-icon-check-line fr-btn--icon-left "]').click()
-            if count % 10 == 0:
-                driver.quit()
-                break
-        finally:
-            send_to_telegram(
-                "Свободные места на визу Франция https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
-                "%20de%20visa")
-            driver.save_screenshot(f'WARNING!!!!!{count}.png')
-
+    try:
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, '//p[@class="lead fr-text mt-4 mb-3 text-center"]')))
         send_to_telegram_log(count)
         count = count + 1
+
+    except:
+        send_to_telegram(
+            "Свободные места на визу Франция https://consulat.gouv.fr/ambassade-de-france-a-minsk/rendez-vous?name=R%C3%A9ception%20des%20demandes"
+            "%20de%20visa")
+        driver.save_screenshot(f'WARNING!!!!!{count}.png')
+
+    driver.quit()
 
 while True:
     checker()
